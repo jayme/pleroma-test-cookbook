84718c01949cb0a2db14667bd9a3ad3fab4cd8b2
jayme <jayme.poyner@gmail.com>
2018-12-14 09:19:32 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# pleroma_test_cookbook
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: pleroma_test_cookbook
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
